84cc7fff5f760103ae4acaba18f58e52d09bf205
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-19 18:56:47 +0300

    add todo now working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import { useState } from "react";
 import "./App.css";
 
-const todos = {
-  todos: [
-    {
-      text: "watch new lesson",
-      id: 15,
-      created_at: "2023-07-05T15:14:15.080Z",
-      user: null,
-    },
-    {
-      text: "pass hw",
-      id: 16,
-      created_at: "2023-07-05T15:14:42.499Z",
-      user: null,
-    },
-    {
-      text: "run training",
-      id: 17,
-      created_at: "2023-07-05T15:14:48.499Z",
-      user: null,
-    },
-    {
-      text: "reading",
-      id: 18,
-      created_at: "2023-07-05T15:14:49.739Z",
-      user: null,
-    },
-  ],
-};
+// const todos = ;
 
 function App() {
   const [value, setValue] = useState("");
+  const [todos, setTodos] = useState({
+    todos: [
+      {
+        text: "watch new lesson",
+        id: 15,
+        created_at: "2023-07-05T15:14:15.080Z",
+        user: null,
+      },
+      {
+        text: "pass hw",
+        id: 16,
+        created_at: "2023-07-05T15:14:42.499Z",
+        user: null,
+      },
+      {
+        text: "run training",
+        id: 17,
+        created_at: "2023-07-05T15:14:48.499Z",
+        user: null,
+      },
+      {
+        text: "reading",
+        id: 18,
+        created_at: "2023-07-05T15:14:49.739Z",
+        user: null,
+      },
+    ],
+  });
 
   const addTodo = () => {
-    alert(value);
+    setTodos({
+      todos: [
+        ...todos.todos,
+        {
+          text: value,
+          id: Date.now(),
+          created_at: new Date(),
+          user: null,
+        },
+      ],
+    });
+    setValue("");
   };
   return (
     <>
