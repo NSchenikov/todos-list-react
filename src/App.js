f78165cbf7a845ba2d02508324374453bfde3422
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-19 19:17:19 +0300

    render todos list from api

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// const todos = ;
+const fetchTodos = () => {
+  return fetch("https://webdev-api.sky.pro/api/todos").then((res) =>
+    res.json()
+  );
+};
 
 function App() {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState({
-    todos: [
-      {
-        text: "watch new lesson",
-        id: 15,
-        created_at: "2023-07-05T15:14:15.080Z",
-        user: null,
-      },
-      {
-        text: "pass hw",
-        id: 16,
-        created_at: "2023-07-05T15:14:42.499Z",
-        user: null,
-      },
-      {
-        text: "run training",
-        id: 17,
-        created_at: "2023-07-05T15:14:48.499Z",
-        user: null,
-      },
-      {
-        text: "reading",
-        id: 18,
-        created_at: "2023-07-05T15:14:49.739Z",
-        user: null,
-      },
-    ],
+    todos: [],
   });
+
+  useEffect(() => {
+    fetchTodos().then((data) => {
+      setTodos(data);
+    });
+  }, []);
 
   const addTodo = () => {
     setTodos({
