4e0323c9f4be4cccfbef3b353ad21a98634f49f4
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-08-19 18:07:31 +0300

    add todo object. Render todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const todos = {
+  todos: [
+    {
+      text: "watch new lesson",
+      id: 15,
+      created_at: "2023-07-05T15:14:15.080Z",
+      user: null,
+    },
+    {
+      text: "pass hw",
+      id: 16,
+      created_at: "2023-07-05T15:14:42.499Z",
+      user: null,
+    },
+    {
+      text: "run training",
+      id: 17,
+      created_at: "2023-07-05T15:14:48.499Z",
+      user: null,
+    },
+    {
+      text: "reading",
+      id: 18,
+      created_at: "2023-07-05T15:14:49.739Z",
+      user: null,
+    },
+  ],
+};
 
 function App() {
+  const addTodo = () => {
+    alert("clicked");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {todos.todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+      <input />
+      <button onClick={addTodo}>Add</button>
+    </>
   );
 }
 
